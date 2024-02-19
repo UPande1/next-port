b82a57e7cb2da9b045b42db26e954d949e315e87
Utkarsh <utkarshpande342@gmail.com>
2024-02-19 22:13:16 +0530

    final try

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,22 +2,22 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      to: ["utkarshpande342@gmail.com"],
-      subject: "Hiring Me",
-      react: (
-        <>
-          <p>Email Body res</p>
-        </>
-      ),
-    });
+  // try {
+  //   const data = await resend.emails.send({
+  //     to: ["utkarshpande342@gmail.com"],
+  //     subject: "Hiring Me",
+  //     react: (
+  //       <>
+  //         <p>Email Body res</p>
+  //       </>
+  //     ),
+  //   });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
+  //   return NextResponse.json(data);
+  // } catch (error) {
+  //   return NextResponse.json({ error });
+  // }
 }
