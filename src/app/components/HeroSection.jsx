f815993be4489023ee1d3d5d0c90104ee0df6715
Utkarsh Pande <95009270+UPande1@users.noreply.github.com>
2024-02-22 11:49:42 +0530

    Update HeroSection.jsx
    updated Resume

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/187QnQdJPQA2KwTRwCvhJ3xdNTh66DRut/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1aNm8cU4R-95bsMGa2IwhQJRaA0vO0xw-/view?usp=drive_link"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
